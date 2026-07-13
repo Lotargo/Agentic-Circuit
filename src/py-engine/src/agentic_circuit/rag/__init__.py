9a6dce496139b45b36e5f71c5b6477c2a39565e2
Boiko Oleg <etot.mir.realen@gmail.com>
2026-07-14 02:55:26 +0300

    feat: export typed memory hits

--- src/py-engine/src/agentic_circuit/rag/__init__.py
+++ src/py-engine/src/agentic_circuit/rag/__init__.py
@@ -3,12 +3,13 @@
 from .bm25 import BM25Index
 from .embeddings import EmbeddingClient
 from .rerank import RerankClient
-from .store import NullMemory, VectorMemory
+from .store import MemoryHit, NullMemory, VectorMemory
 
 __all__ = [
     "BM25Index",
     "EmbeddingClient",
     "RerankClient",
+    "MemoryHit",
     "NullMemory",
     "VectorMemory",
 ]
